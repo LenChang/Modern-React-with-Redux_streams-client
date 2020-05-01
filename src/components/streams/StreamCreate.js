b4f7488be84e13b2d2fd168430957933956f8081
Len Chang <lamgo.chang@gmail.com>
2020-05-01 22:10:58 +0800

    Modern React with Redux: Section 18 - 233 to 234 - Customizing Form Fields

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,20 +2,36 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
+  renderInput({ input, label }) {
+    // --- long code
+    // return (
+    //   <input
+    //     onChange={formProps.input.onChange}
+    //     value={formProps.input.value}
+    //   ></input>
+    // );
+
+    // --- short code
+    // return <input {...formProps.input} />;
+
+    // --- the shortest code
     return (
-      <input
-        onChange={formProps.input.onChange}
-        value={formProps.input.value}
-      ></input>
+      <div className="filed">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
     );
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
       </form>
     );
   }
