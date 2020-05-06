79ed6cee769b7b43e1984aefc9f9da659dece2f2
Len Chang <lamgo.chang@gmail.com>
2020-05-06 15:05:23 +0800

    Modern React with Redux: Section 19 - 238 Showing Errors on Touch - 239 Highlighting Errored Fields - CSS style about "error"

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,7 +2,19 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label, meta }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+
     // console.log(meta);
 
     // --- long code
@@ -18,13 +30,13 @@ class StreamCreate extends React.Component {
 
     // --- the shortest code
     return (
-      <div className="filed">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValues) {
     console.log(formValues);
@@ -34,7 +46,7 @@ class StreamCreate extends React.Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter title" />
         <Field
